913d34ac009858cc6341c17615b7313d585caa2c
w70701 <w70701@KM109-15>
2025-03-24 18:00:54 +0100

    Dodaj pliki projektów.

---
+++ consoleapplication5/czytelnik.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApplication5
+{
+    public class Czytelnik
+    {
+        const int MAX_LICZBA_WYPOZYCZONYCH = 2;
+        public Czytelnik(string imie, string nazwisko, int id)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Id = id;
+            KsiazkiWypozyczone = new List<int>();
+        }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int Id { get; set; }
+        public List<int> KsiazkiWypozyczone { get; set; }
+
+        public bool Wypozycz(Ksiazka ksiazka)
+        {
+            if (ksiazka.CzyAktywna() && CzyMogeWypozyczyc())
+            {
+                KsiazkiWypozyczone.Add(ksiazka.Id);
+                ksiazka.Wypozycz();
+                return true;
+            }
+            return false;
+        }
+
+        public bool CzyMogeWypozyczyc()
+        {
+            return KsiazkiWypozyczone.Count < MAX_LICZBA_WYPOZYCZONYCH;
+        }
+
+    }
+}
---
+++ consoleapplication5/ksiazka.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApplication5
+{
+    public class Ksiazka
+    {
+        public int Id { get; set; }
+        public string Tytul { get; set; }
+        public int IloscStron { get; set; }
+        public static List<string> Autorzy { get; set; }
+
+        public enum StatusKsiazki
+        {
+            Zagubiono = 0,
+            Aktywna = 1,
+            Czytelnia = 2,
+            Wypozyczona = 3,
+        }
+
+        public StatusKsiazki Status { get; set; }
+
+        public void Wypozycz()
+        {
+            Status = StatusKsiazki.Wypozyczona;
+        }
+        public void Zwrot()
+        {
+            Status = StatusKsiazki.Aktywna;
+        }
+
+        public bool CzyAktywna() =>  Status == StatusKsiazki.Aktywna;
+        
+    }
+}
