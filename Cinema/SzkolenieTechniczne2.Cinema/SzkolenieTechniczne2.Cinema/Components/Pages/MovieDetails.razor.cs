ec343a9390d184f89f8a5e0bf401ad9a3ad2ec17
Pattxx <w70701@student.wsiz.edu.pl>
2025-06-16 17:27:30 +0200

    MovieDetails Razor page

---
+++ Cinema/SzkolenieTechniczne2.Cinema/SzkolenieTechniczne2.Cinema/Components/Pages/MovieDetails.razor.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Components;
+using SzkolenieTechniczne2.Cinema.Domain.Command;
+using SzkolenieTechniczne2.Cinema.Domain.Entities;
+using SzkolenieTechniczne2.Cinema.Domain.Query.Dtos;
+using SzkolenieTechniczne2.Cinema.Domain.Query.Movie.GetMovieCategories;
+using SzkolenieTechniczne2.Cinema.Domain.Query.Movie.GetMovieQuery;
+using SzkolenieTechniczne2.Cinema.Models;
+
+namespace SzkolenieTechniczne2.Cinema.Components.Pages
+{
+    public partial class MovieDetails : ComponentBase
+    {
+        [Inject]
+        public IMediator MediatorService { get; set; } = default;
+        [Inject]
+        public NavigationManager Navigation { get; set; } = default;
+        /// <summary>
+        /// [Parameter]
+        /// </summary>
+        private long MovieId { get; set; }
+
+        private MovieDetailsDto? Details;
+       
+
+        protected override async Task OnInitializedAsync()
+        {
+            Details = await MediatorService.Send(new GetMovieQuery(MovieId));
+        }
+
+
+        private void GoBack() {
+            Navigation.NavigateTo("/movies");
+        }
+
+    }
+}
